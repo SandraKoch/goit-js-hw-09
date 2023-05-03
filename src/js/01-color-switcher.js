d7b99152518f3fcb385513c56928decedbbd539f
SandraKoch <sandraannakoch@gmail.com>
2023-05-03 19:31:39 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function toggleButtons(active, inactive) {
+  inactive.setAttribute('disabled', '');
+  active.removeAttribute('disabled');
+}
+startButtonEl.addEventListener('click', () => {
+  changeBgColor();
+  intervalId = setInterval(changeBgColor, 1000);
+  toggleButtons(stopButtonEl, startButtonEl);
+});
+
+stopButtonEl.addEventListener('click', () => {
+  clearInterval(intervalId);
+  toggleButtons(startButtonEl, stopButtonEl);
+});
